3c5680f86fddb6bda481f52576bd7c76f1c0ea9a
smitpatel24 <smit@gopikunj.com>
2023-05-19 00:50:00 -0700

    Add support for JWT on all pages

--- Frontend/engr-285/src/components/Profile.jsx
+++ Frontend/engr-285/src/components/Profile.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    let authorization = localStorage.getItem("token");
+    if (!authorization) {
+      navigate("/login");
+    } else {
+      console.log("On profile page");
+      // Do something here
+    }
+  }, []);
   return <div>Profile</div>;
 };
 
--- Frontend/engr-285/src/components/YourPosts.jsx
+++ Frontend/engr-285/src/components/YourPosts.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const YourPosts = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    let authorization = localStorage.getItem("token");
+    if (!authorization) {
+      navigate("/login");
+    } else {
+      console.log("On your posts page");
+      // Do something here
+    }
+  }, []);
   return <div>YourPosts</div>;
 };
 
