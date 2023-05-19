db5c2ad19085ce4ffd1c54c89ff9dea739576dea
smitpatel24 <smit@gopikunj.com>
2023-05-19 00:59:00 -0700

    Add logout confirmation

--- Frontend/engr-285/src/components/BottomBar.jsx
+++ Frontend/engr-285/src/components/BottomBar.jsx
@@ -7,10 +7,36 @@ import PersonIcon from "@mui/icons-material/Person";
 import ListAltIcon from "@mui/icons-material/ListAlt";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { useNavigate } from "react-router-dom";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@mui/material";
 
 export default function BottomBar() {
   const [value, setValue] = React.useState(0);
+  const [logoutDialogOpen, setLogoutDialogOpen] = React.useState(false);
   const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/sign-in");
+  };
+
+  const handleLogoutConfirmation = () => {
+    setLogoutDialogOpen(true);
+  };
+
+  const handleLogoutCancel = () => {
+    setLogoutDialogOpen(false);
+  };
+
+  const handleLogoutConfirm = () => {
+    setLogoutDialogOpen(false);
+    handleLogout();
+  };
 
   return (
     <Box sx={{ width: "100%", position: "absolute", bottom: 0 }}>
@@ -36,8 +62,29 @@ export default function BottomBar() {
           icon={<ListAltIcon />}
           onClick={() => navigate("/yourPosts")}
         />
-        <BottomNavigationAction label="Logout" icon={<LogoutIcon />} />
+        <BottomNavigationAction
+          label="Logout"
+          icon={<LogoutIcon />}
+          onClick={handleLogoutConfirmation}
+        />
       </BottomNavigation>
+
+      <Dialog open={logoutDialogOpen} onClose={handleLogoutCancel}>
+        <DialogTitle>Logout Confirmation</DialogTitle>
+        <DialogContent>
+          <p>Are you sure you want to logout?</p>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleLogoutCancel}>Cancel</Button>
+          <Button
+            onClick={handleLogoutConfirm}
+            variant="contained"
+            color="error"
+          >
+            Logout
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 }
